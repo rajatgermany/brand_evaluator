219b8c3a4fd43eb73f0c31808b9d42bd9c2cb7c1
Rajat Jain <rajatuiet@gmail.com>
2019-10-15 16:57:13 +0200

    cleaned the code

--- backend/src/app/db/session.py
+++ backend/src/app/db/session.py
@@ -3,12 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 
-a = 'postgresql://postgres:changethis@34.65.154.15:5432/app'
-print('a', a)
-# engine = create_engine(config.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
-engine = create_engine(a, pool_pre_ping=True)
-
-
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
 db_session = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine)
 )
--- frontend/src/services/TweetsService.js
+++ frontend/src/services/TweetsService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 class TweetService {
     static getsentiment(query, successCallback, errorCallback) {
         query = "#"+query 
-        const url = 'http://34.65.245.120:80/api_v1/tweet_analyzer/'
+        const url = 'http://localhost:80/api_v1/tweet_analyzer/'
         console.log('it hits here', url)
         axios({
             method: 'post',
